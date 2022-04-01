0e1b64198af59f5835f602e1191d8c38510cf51d
Terry Geunbin Kim <terry.kim@nexr.com>
2022-04-01 17:00:10 +0900

    push

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+import FollowerList from "../components/FollowerList";
+
 
 const Profile = () => {
+    const followList = [{nickname: "terry"}, {nickname: 'terry2'}];
+    const followerList = [{nickname: "terry"}, {nickname: 'terry2'}];
     return (
         <>
             <Head>
                 <title>프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>프로필</div>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followList}  />
+                <FollowerList header="팔로워 목록" data={followerList} />
             </AppLayout>
         </>
     );
